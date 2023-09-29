6696dd3bc00a45e4ada28f9afc22a5f27d6cee9f
Patrick Jahr <patrick.jahr@thinktecture.com>
2023-09-29 12:48:09 +0200

    state until pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,83 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { PaintService } from "./paint.service";
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {PaintService} from "./paint.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  // LAB #1
+    // LAB #1
+    @ViewChild('canvas') canvas?: ElementRef<HTMLCanvasElement>;
+    // LAB #2.1
+    context?: CanvasRenderingContext2D;
 
-  // LAB #2.1
-
-  // LAB #5
-
-  // LAB #11
-
-  // LAB #17
-
-  constructor(private readonly paintService: PaintService) {
-  }
-
-  ngAfterViewInit(): void {
-    // LAB #2.2, 2.3 and 3
-
-    // LAB #16
-  }
-
-  onPointerDown(event: PointerEvent) {
     // LAB #5
-  }
+    previousPoint?: { x: number, y: number };
 
-  onPointerMove(event: PointerEvent) {
-    // LAB #4 and 5
-  }
-
-  onPointerUp() {
-    // LAB #5
-  }
-
-  onColorChange(color: HTMLInputElement) {
-    // LAB #6
-  }
-
-  async open() {
-    // LAB #12
-  }
-
-  async save() {
     // LAB #11
-  }
 
-  async copy() {
-    // LAB #13
-  }
+    // LAB #17
 
-  async paste() {
-    // LAB #14
-  }
+    constructor(private readonly paintService: PaintService) {
+    }
 
-  async share() {
-    // LAB #15
-  }
+    ngAfterViewInit(): void {
+        // LAB #2.2, 2.3 and 3
+        const canvas = this.canvas?.nativeElement;
+        const ctx = canvas!.getContext('2d', {desynchronized: true,})!;
+        this.context = ctx;
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas!.width, canvas!.height);
+        ctx.fillStyle = 'black';
+
+        // LAB #16
+    }
+
+    onPointerDown(event: PointerEvent) {
+        // LAB #5
+        this.previousPoint = {x: event.offsetX, y: event.offsetY};
+    }
+
+    onPointerMove(event: PointerEvent) {
+        // LAB #4 and 5
+        if (this.previousPoint) {
+            const currentPoint = {x: event.offsetX, y: event.offsetY};
+            for (const point of this.paintService.bresenhamLine(this.previousPoint, currentPoint)){
+                this.context!.fillRect(point.x, point.y, 2, 2);
+            }
+            this.previousPoint = currentPoint;
+        }
+    }
+
+    onPointerUp() {
+        // LAB #5
+        this.previousPoint = undefined;
+    }
+
+    onColorChange(color: HTMLInputElement) {
+        // LAB #6
+        this.context!.fillStyle = color.value;
+    }
+
+    async open() {
+        // LAB #12
+    }
+
+    async save() {
+        // LAB #11
+    }
+
+    async copy() {
+        // LAB #13
+    }
+
+    async paste() {
+        // LAB #14
+    }
+
+    async share() {
+        // LAB #15
+    }
 }
